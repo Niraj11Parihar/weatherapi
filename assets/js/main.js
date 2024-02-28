1deb08341063d9d2befeafa7e6389a88d5826f8a
Niraj Parihar <148898086+Niraj11Parihar@users.noreply.github.com>
2024-02-28 23:07:23 +0530

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+var config = {
+    curl: "https://api.countrystatecity.in/v1/countries",
+    ckey: "NHhvOEcyWk50N2Vna3VFTE00bFp3MjFKR0ZEOUhkZlg4RTk1MlJlaA=="
+}
+
+
+var country = document.querySelector(".country")
+var country = document.querySelector(".state")
+var country = document.querySelector(".city")
+
+function countryload(){
+    
+}
